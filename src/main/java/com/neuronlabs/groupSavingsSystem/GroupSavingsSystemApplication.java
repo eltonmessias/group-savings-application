a81ee74c86328be7be93e87028100aad90b0b828
elton <eltonguambe1006@gmail.com>
2025-07-22 19:22:49 +0200

    first commit

---
+++ src/main/java/com/neuronlabs/groupSavingsSystem/GroupSavingsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.neuronlabs.groupSavingsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSavingsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupSavingsSystemApplication.class, args);
+	}
+
+}
